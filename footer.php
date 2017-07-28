b6dbba2c9c4a0b3d0e54eeff2014c6a4182041be
Whittney Twomey <wtwome13@gmail.com>
2017-07-27 17:26:19 -0700

    added files to create homepage

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!-- Footer -->
+<footer>
+
+<!-- Decorative-Bottom -->
+  <h2 class="decorative"><span><img src="img/beach-ball-sm.png"></span></h2>
+  <hr class="decoration-bottom">
+  <hr class="decoration-bottom2">
+
+  <div class="footer">
+    <a href="">About Us</a>
+    <a href="">Our Team</a>
+    <a href="">Locations</a>
+    <a href="">FAQ</a>
+  </div>
+  <div class="copyright">
+    <i class="fa fa-copyright"> 2017 Beach Babies, LLC</i>
+  </div>
+</footer>
+</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+  	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="stylesheets/stylesheet.css" type="text/css">
+    <link rel="stylesheet" href="stylesheets/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="stylesheets/font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <!-- Javascript/jQuery -->
+    <script type="text/javascript" src="stylesheets/jquery-3.2.1.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <script type="text/javascript" src="stylesheets/beach.js"></script>
+    <script type="text/javascript" src="stylesheets/bootstrap.min.js"></script>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Ubuntu:300,400" rel="stylesheet">
+
+    <!-- Title -->
+  	<title>Beach Babies, LLC - Welcome</title>
+
+  </head>
+
+<body>
+  <!-- Navigation -->
+    <div class="menu-section">
+      <div class="menu-toggle">
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+      </div>
+      <nav>
+        <ul role="navigation" class="hidden">
+          <li><a href="">home</a></li>
+          <li><a href="">admission</a></li>
+          <li><a href="">programs</a></li>
+          <li><a href="">locations</a></li>
+          <li><a href="">calendar</a></li>
+        </ul>
+      </nav>
+    </div>
+
+  <!-- Container -->
+  <div class="container">
+    <!-- Header -->
+    <header>
+      <!-- Logo -->
+      <div class="logo">
+        <div class="logo-container">
+          <a class="link-home" href="">
+  					Beach
+  							<img src="img/boat.png">
+  					Babies
+  				</a>
+        </div>
+      </div>
+    </header>
